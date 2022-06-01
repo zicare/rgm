04ac0e898ddd1977d047e175869db72b3698069b
Andrés Barreto <zicare@gmail.com>
2022-05-31 19:02:10 -0500

    Added support for Mysql and Postgres in db package

--- db/db.go
+++ db/db.go
@@ -9,27 +9,37 @@ import (
 
 	//required
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 )
 
 var db *sql.DB
 
 //Init tests the db connection and saves the db handler
-func Init() error {
+func Init(flavor string) error {
 
 	var (
-		err error
-		c   = config.Config()
-		//conn = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		conn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
-			c.GetString("db.user"),
-			c.GetString("db.password"),
-			c.GetString("db.host"),
-			c.GetString("db.port"),
-			c.GetString("db.name"))
+		err  error
+		conn string
+		c    = config.Config()
 	)
 
-	//db, err = sql.Open("postgres", conn)
-	db, err = sql.Open("mysql", conn)
+	switch flavor {
+	case "mysql":
+		conn = "%s:%s@tcp(%s:%s)/%s?parseTime=true"
+	case "postgres":
+		conn = "postgres://%s:%s@%s:%s/%s?sslmode=disable"
+	default:
+		return msg.Get("25").SetArgs(err.Error()).M2E()
+	}
+
+	conn = fmt.Sprintf(conn,
+		c.GetString("db.user"),
+		c.GetString("db.password"),
+		c.GetString("db.host"),
+		c.GetString("db.port"),
+		c.GetString("db.name"))
+
+	db, err = sql.Open(flavor, conn)
 	if err != nil {
 		//Server error: %s
 		return msg.Get("25").SetArgs(err.Error()).M2E()
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/lib/pq v1.10.6
 	github.com/spf13/viper v1.11.0
 )
 
